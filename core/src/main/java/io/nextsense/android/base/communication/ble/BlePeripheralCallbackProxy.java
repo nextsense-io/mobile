c71d3acfb207907584a0ad38b34d9e9681bc9020
Eric Bouchard <eric@nextsense.io>
2021-07-22 14:52:34 +0700

    Add synchronous Bluetooth write and read functions.

--- core/src/main/java/io/nextsense/android/base/communication/ble/BlePeripheralCallbackProxy.java
+++ core/src/main/java/io/nextsense/android/base/communication/ble/BlePeripheralCallbackProxy.java
@@ -3,23 +3,33 @@ package io.nextsense.android.base.communication.ble;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.util.ArraySet;
-import android.util.Log;
 
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
 import com.welie.blessed.BluetoothPeripheral;
 import com.welie.blessed.BluetoothPeripheralCallback;
 import com.welie.blessed.GattStatus;
 import com.welie.blessed.PhyType;
+import com.welie.blessed.WriteType;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
- * Created by Eric Bouchard on 7/12/2021.
+ * Proxies access to the BluetoothPeripheralCallback object so that multiple objects can listens to
+ * the events that are relevant to them.
+ *
+ * Also provides a few convenience methods to do Bluetooth operations synchronously using futures.
  */
 public class BlePeripheralCallbackProxy {
 
   private final Set<BluetoothPeripheralCallback> componentCallbacks = new ArraySet<>();
+  private final Map<String, SettableFuture<byte[]>> writeFutures = new HashMap<>();
+  private final Map<String, SettableFuture<byte[]>> readFutures = new HashMap<>();
 
   /**
    * Gets the main device callback that should be set in the Bluetooth stack.
@@ -35,6 +45,45 @@ public class BlePeripheralCallbackProxy {
   public void removePeripheralCallbackListener(BluetoothPeripheralCallback callback) {
     componentCallbacks.remove(callback);
   }
+
+  /**
+   * Read a characteristic for a peripheral and return a Future that will be completed when it is
+   * confirmed by the Android stack.
+   */
+  public synchronized ListenableFuture<byte[]> readCharacteristic(
+      BluetoothPeripheral peripheral, BluetoothGattCharacteristic characteristic) {
+    if (readFutures.get(peripheral.getAddress()) != null &&
+        !readFutures.get(peripheral.getAddress()).isDone()) {
+      // Should call this function one by one for a peripheral as it is sync.
+      return Futures.immediateCancelledFuture();
+    }
+    SettableFuture<byte[]> readFuture = SettableFuture.create();
+    readFutures.put(peripheral.getAddress(), readFuture);
+    peripheral.readCharacteristic(characteristic);
+    return readFuture;
+  }
+
+  /**
+   * Write a characteristic for a peripheral and return a Future that will be completed when it is
+   * confirmed by the Android stack. This implies that it is of type WRITE_WITH_RESPONSE.
+   */
+  public synchronized ListenableFuture<byte[]> writeCharacteristic(
+      BluetoothPeripheral peripheral, BluetoothGattCharacteristic characteristic, byte[] value) {
+    if (writeFutures.get(peripheral.getAddress()) != null &&
+        !writeFutures.get(peripheral.getAddress()).isDone()) {
+      // Should call this function one by one for a peripheral as it is sync.
+      return Futures.immediateCancelledFuture();
+    }
+    SettableFuture<byte[]> writeFuture = SettableFuture.create();
+    writeFutures.put(peripheral.getAddress(), writeFuture);
+    boolean writeDone = peripheral.writeCharacteristic(characteristic, value, WriteType.WITH_RESPONSE);
+    if (!writeDone) {
+      return Futures.immediateFailedFuture(new BluetoothException(
+          "Failed to write to characteristic " + characteristic.getUuid()));
+    }
+    return writeFuture;
+  }
+
 
   private final BluetoothPeripheralCallback deviceCallback = new BluetoothPeripheralCallback() {
     @Override
@@ -57,8 +106,20 @@ public class BlePeripheralCallbackProxy {
     public void onCharacteristicUpdate(
         @NotNull BluetoothPeripheral peripheral, @NotNull byte[] value,
         @NotNull BluetoothGattCharacteristic characteristic, @NotNull GattStatus status) {
-      for (BluetoothPeripheralCallback callback : componentCallbacks) {
-        callback.onCharacteristicUpdate(peripheral, value, characteristic, status);
+      SettableFuture<byte[]> readFuture = readFutures.get(peripheral.getAddress());
+      if (readFuture != null && !readFuture.isDone()) {
+        // Return a response on the future.
+        if (status == GattStatus.SUCCESS) {
+          readFuture.set(value);
+        } else {
+          readFuture.setException(
+              new BluetoothException("Failed to read with status " + status.toString()));
+        }
+      } else {
+        // Use the async callback instead.
+        for (BluetoothPeripheralCallback callback : componentCallbacks) {
+          callback.onCharacteristicUpdate(peripheral, value, characteristic, status);
+        }
       }
     }
 
@@ -66,8 +127,20 @@ public class BlePeripheralCallbackProxy {
     public void onCharacteristicWrite(
         @NotNull BluetoothPeripheral peripheral, @NotNull byte[] value,
         @NotNull BluetoothGattCharacteristic characteristic, @NotNull GattStatus status) {
-      for (BluetoothPeripheralCallback callback : componentCallbacks) {
-        callback.onCharacteristicWrite(peripheral, value, characteristic, status);
+      SettableFuture<byte[]> writeFuture = writeFutures.get(peripheral.getAddress());
+      if (writeFuture != null && !writeFuture.isDone()) {
+        // Return a response on the future.
+        if (status == GattStatus.SUCCESS) {
+          writeFuture.set(value);
+        } else {
+          writeFuture.setException(
+              new BluetoothException("Failed to write with status " + status.toString()));
+        }
+      } else {
+        // Use the async callback instead.
+        for (BluetoothPeripheralCallback callback : componentCallbacks) {
+          callback.onCharacteristicWrite(peripheral, value, characteristic, status);
+        }
       }
     }
 
