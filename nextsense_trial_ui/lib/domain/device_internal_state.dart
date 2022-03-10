433d815a1ef636c5921fbe8a3ef58e595351e5a1
Eric Bouchard <eric@nextsense.io>
2022-03-10 13:59:09 +0700

    Parse DeviceInternalState gson in Flutter
    Changed short to int as negative values were converted to strings and this caused issues when parsing the objects.

--- nextsense_trial_ui/lib/domain/device_internal_state.dart
+++ nextsense_trial_ui/lib/domain/device_internal_state.dart
@@ -1,6 +1,8 @@
 
 // Fields list for the DeviceInternalState object received from the native
 // layer.
+import 'package:flutter/foundation.dart';
+
 enum Fields {
   // When the state was sampled in the hardware device.
   timestamp,
@@ -35,4 +37,46 @@ enum Fields {
   // Boolean array indicating if a channel lead is considered to the off
   // (Impedance too high). Channels 1-8 in order.
   leadsOffPositive
+}
+
+class DeviceInternalState {
+
+  DateTime timestamp;
+  int batteryMilliVolts;
+  bool busy;
+  bool uSdPresent;
+  bool hdmiCablePresent;
+  bool rtcClockSet;
+  bool captureRunning;
+  bool charging;
+  bool batteryLow;
+  bool uSdLoggingEnabled;
+  bool internalErrorDetected;
+  int samplesCounter;
+  int bleQueueBacklog;
+  int lostSamplesCounter;
+  int bleRssi;
+  List<bool> leadsOffPositive;
+
+  DeviceInternalState(Map<String, dynamic> values) :
+      timestamp = DateTime.parse(values[describeEnum(Fields.timestamp)]),
+      batteryMilliVolts = values[describeEnum(Fields.batteryMilliVolts)].value,
+      busy = values[describeEnum(Fields.busy)],
+      uSdPresent = values[describeEnum(Fields.uSdPresent)],
+      hdmiCablePresent = values[describeEnum(Fields.hdmiCablePresent)],
+      rtcClockSet = values[describeEnum(Fields.rtcClockSet)],
+      captureRunning = values[describeEnum(Fields.captureRunning)],
+      charging = values[describeEnum(Fields.charging)],
+      batteryLow = values[describeEnum(Fields.batteryLow)],
+      uSdLoggingEnabled = values[describeEnum(Fields.uSdLoggingEnabled)],
+      internalErrorDetected =
+          values[describeEnum(Fields.internalErrorDetected)],
+      samplesCounter = values[describeEnum(Fields.samplesCounter)].value,
+      bleQueueBacklog = values[describeEnum(Fields.bleQueueBacklog)].value,
+      lostSamplesCounter =
+          values[describeEnum(Fields.lostSamplesCounter)].value,
+      bleRssi = values[describeEnum(Fields.bleRssi)].value,
+      leadsOffPositive = new List<bool>.from(
+          values[describeEnum(Fields.leadsOffPositive)])
+  {}
 }
