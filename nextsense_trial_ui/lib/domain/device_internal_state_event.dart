7cbaaff55a27dc84ce156f56a2da5e29d7ba176c
Alex Rossovsky <rlexey@gmail.com>
2022-03-15 13:09:01 +0700

    Implemented device internal state event system
    1) Added DeviceInternalStateEvent happening when some internal state changed
    2) Added DeviceStateViewModel as base model for different ui parts

---
+++ nextsense_trial_ui/lib/domain/device_internal_state_event.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/foundation.dart';
+import 'package:nextsense_trial_ui/domain/device_internal_state.dart';
+
+enum DeviceInternalStateEventType {
+  hdmiCableConnected,
+  hdmiCableDisconnected,
+  uSdConnected,
+  uSdDisconnected,
+  unknown
+}
+
+class DeviceInternalStateEvent {
+  final DeviceInternalStateFields field;
+  final dynamic value;
+  final DeviceInternalStateEventType type;
+
+  const DeviceInternalStateEvent(this.field, this.value, this.type);
+
+  factory DeviceInternalStateEvent.create(String fieldKey, dynamic value) {
+    DeviceInternalStateFields field =
+      DeviceInternalStateFields.values.asNameMap()[fieldKey]!;
+    DeviceInternalStateEventType type = DeviceInternalStateEventType.unknown;
+
+    // TODO(alex): add more event mapping
+    switch (field) {
+      case DeviceInternalStateFields.uSdPresent:
+        type = value ? DeviceInternalStateEventType.uSdConnected :
+        DeviceInternalStateEventType.uSdDisconnected;
+        break;
+      case DeviceInternalStateFields.hdmiCablePresent:
+        type = value ? DeviceInternalStateEventType.hdmiCableConnected :
+        DeviceInternalStateEventType.hdmiCableDisconnected;
+        break;
+      default:
+        break;
+    }
+
+    return DeviceInternalStateEvent(field, value, type);
+  }
+
+  @override
+  bool operator==(Object other) {
+      if (identical(this, other))
+        return true;
+
+      if (!(other is DeviceInternalStateEvent))
+        return false;
+
+      if (this.field != other.field)
+        return false;
+
+      if (this.value is List && other.value is List)
+        return listEquals(this.value, other.value);
+
+      return this.value == other.value;
+  }
+
+  @override
+  String toString() {
+    return "DeviceInternalStateChangeEvent(${this.type})";
+  }
+
+}
---
+++ nextsense_trial_ui/lib/viewmodels/device_state_vm.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+import 'package:logging/logging.dart';
+import 'package:nextsense_trial_ui/di.dart';
+import 'package:nextsense_trial_ui/domain/device_internal_state_event.dart';
+import 'package:nextsense_trial_ui/managers/device_manager.dart';
+import 'package:nextsense_trial_ui/utils/android_logger.dart';
+
+abstract class DeviceStateViewModelInterface extends ChangeNotifier {
+  void onDeviceDisconnected();
+  void onDeviceReconnected();
+  void onDeviceInternalStateChanged(DeviceInternalStateEvent event);
+}
+
+abstract class DeviceStateViewModel
+    extends DeviceStateViewModelInterface {
+
+  final CustomLogPrinter _logger = CustomLogPrinter('DeviceStateViewModel');
+  final DeviceManager _deviceManager = getIt<DeviceManager>();
+
+  DeviceState get deviceState => _deviceManager.deviceState.value;
+  bool get deviceIsConnected => deviceState == DeviceState.READY;
+
+  StreamSubscription<DeviceInternalStateEvent>?
+    _deviceInternalStateChangesSubscription;
+
+  // Internal state shortcuts
+  bool get isHdmiCablePresent => _deviceManager.isHdmiCablePresent;
+  bool get isUSdPresent => _deviceManager.isUSdPresent;
+
+  void init() {
+    _deviceManager.deviceState.addListener(_onDeviceStateChanged);
+
+    _deviceInternalStateChangesSubscription = _deviceManager
+        .deviceInternalStateChangeStream.listen(onDeviceInternalStateChanged);
+  }
+
+  void _onDeviceStateChanged() {
+    switch (deviceState) {
+      case DeviceState.DISCONNECTED:
+        onDeviceDisconnected();
+        break;
+      case DeviceState.READY:
+        onDeviceReconnected();
+        break;
+      default:
+        break;
+    }
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _deviceManager.deviceState.removeListener(_onDeviceStateChanged);
+    _deviceInternalStateChangesSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  void onDeviceInternalStateChanged(DeviceInternalStateEvent event) {
+    _logger.log(Level.INFO, "onDeviceInternalStateChanged: $event");
+    notifyListeners();
+  }
+}
