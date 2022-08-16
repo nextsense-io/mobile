9c085fc8534f1157a3b2f8832356f5e88b205130
Eric Bouchard <eric@nextsense.io>
2022-08-16 10:55:24 -0700

    Do not write data to the DB if not uploading
    The memory cache will be used only in that case.

--- core/src/main/java/io/nextsense/android/base/db/DatabaseSink.java
+++ core/src/main/java/io/nextsense/android/base/db/DatabaseSink.java
@@ -9,6 +9,7 @@ import org.greenrobot.eventbus.ThreadMode;
 import java.time.Instant;
 
 import io.nextsense.android.base.data.DeviceInternalState;
+import io.nextsense.android.base.data.LocalSessionManager;
 import io.nextsense.android.base.data.Samples;
 import io.nextsense.android.base.db.objectbox.ObjectBoxDatabase;
 
@@ -20,13 +21,16 @@ public class DatabaseSink {
   private static final String TAG = DatabaseSink.class.getSimpleName();
 
   private final ObjectBoxDatabase boxDatabase;
+  private final LocalSessionManager localSessionManager;
 
-  private DatabaseSink(ObjectBoxDatabase boxDatabase) {
+  private DatabaseSink(ObjectBoxDatabase boxDatabase, LocalSessionManager localSessionManager) {
     this.boxDatabase = boxDatabase;
+    this.localSessionManager = localSessionManager;
   }
 
-  public static DatabaseSink create(ObjectBoxDatabase objectBoxDatabase) {
-    return new DatabaseSink(objectBoxDatabase);
+  public static DatabaseSink create(
+      ObjectBoxDatabase objectBoxDatabase, LocalSessionManager localSessionManager) {
+    return new DatabaseSink(objectBoxDatabase, localSessionManager);
   }
 
   public void startListening() {
@@ -45,15 +49,19 @@ public class DatabaseSink {
 
   @Subscribe(threadMode = ThreadMode.BACKGROUND)
   public void onSamples(Samples samples) {
-    Instant saveStartTime = Instant.now();
-    boxDatabase.runInTx(() -> {
-      boxDatabase.putEegSamples(samples.getEegSamples());
-      boxDatabase.putAccelerations(samples.getAccelerations());
+    localSessionManager.getActiveLocalSession().ifPresent((currentLocalSession) -> {
+      if (currentLocalSession.isUploadNeeded()) {
+        Instant saveStartTime = Instant.now();
+        boxDatabase.runInTx(() -> {
+          boxDatabase.putEegSamples(samples.getEegSamples());
+          boxDatabase.putAccelerations(samples.getAccelerations());
+        });
+        long saveTime = Instant.now().toEpochMilli() - saveStartTime.toEpochMilli();
+        if (saveTime > 20) {
+          Log.d(TAG, "It took " + saveTime + " to write xenon data.");
+        }
+      }
     });
-    long saveTime = Instant.now().toEpochMilli() - saveStartTime.toEpochMilli();
-    if (saveTime > 20) {
-      Log.d(TAG, "It took " + saveTime + " to write xenon data.");
-    }
   }
 
   @Subscribe(threadMode = ThreadMode.BACKGROUND)
