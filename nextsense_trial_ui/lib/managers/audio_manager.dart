f57d99345c5e809f9c826eb8a84e8d994b0f3853
Eric Bouchard <eric@nextsense.io>
2022-09-16 14:27:18 +0700

    Update Gradle version
    Needed to update audioplayers to be compatible, which necessitated code changes.

--- nextsense_trial_ui/lib/managers/audio_manager.dart
+++ nextsense_trial_ui/lib/managers/audio_manager.dart
@@ -1,24 +1,29 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:logging/logging.dart';
+import 'package:nextsense_trial_ui/utils/android_logger.dart';
 
 /// Manage the single audio player for the application and the sound files cache
 /// for low latency playback which is important when running protocols.
 class AudioManager {
-  final AudioPlayer _audioPlayer;
-  late final AudioCache _cachedPlayer;
+  final CustomLogPrinter _logger = CustomLogPrinter('AudioManager');
 
-  AudioManager() : _audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY) {
-    _cachedPlayer = AudioCache(fixedPlayer: _audioPlayer);
-  }
+  final AudioPlayer _audioPlayer;
+
+  AudioManager() : _audioPlayer = AudioPlayer();
 
   Future cacheAudioFile(String assetLocation) async {
-    await _cachedPlayer.load(assetLocation);
+    _logger.log(Level.FINE, "Caching ${assetLocation}");
+    // TODO(eric): Caching is broken in this version, re-enable when fixed.
+    // await _audioPlayer.setSource(AssetSource(assetLocation));
   }
 
   Future playAudioFile(String assetLocation, {bool? loop}) async {
+    _logger.log(Level.FINE, "Playing ${assetLocation}");
+    await _audioPlayer.play(AssetSource(assetLocation));
     if (loop != null && loop) {
-      await _cachedPlayer.loop(assetLocation);
+      await _audioPlayer.setReleaseMode(ReleaseMode.loop);
     } else {
-      await _cachedPlayer.play(assetLocation);
+      await _audioPlayer.setReleaseMode(ReleaseMode.stop);
     }
   }
 
@@ -27,6 +32,6 @@ class AudioManager {
   }
 
   Future clearCache() async {
-    await _cachedPlayer.clearAll();
+    await _audioPlayer.audioCache.clearAll();
   }
 }
