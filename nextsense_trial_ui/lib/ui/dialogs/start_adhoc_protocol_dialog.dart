34955f1bda0d19b396e7d1b95a3fb99f92c6c162
Eric Bouchard <eric@nextsense.io>
2022-09-20 13:53:55 +0700

    Update adhoc popups UI

--- nextsense_trial_ui/lib/ui/dialogs/start_adhoc_protocol_dialog.dart
+++ nextsense_trial_ui/lib/ui/dialogs/start_adhoc_protocol_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nextsense_trial_ui/di.dart';
+import 'package:nextsense_trial_ui/ui/components/header_text.dart';
 import 'package:nextsense_trial_ui/ui/components/medium_text.dart';
 import 'package:nextsense_trial_ui/ui/components/simple_button.dart';
 import 'package:nextsense_trial_ui/ui/dialogs/start_adhoc_protocol_dialog_vm.dart';
@@ -32,8 +33,10 @@ class StartAdhocProtocolDialog extends HookWidget {
         .toList();
 
     return SimpleDialog(
-      title: const Text('Select adhoc protocol to start'),
+      title: HeaderText(text: 'Select adhoc protocol to start'),
       children: options,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))),
     );
   }
 
--- nextsense_trial_ui/lib/ui/dialogs/start_adhoc_survey_dialog.dart
+++ nextsense_trial_ui/lib/ui/dialogs/start_adhoc_survey_dialog.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nextsense_trial_ui/di.dart';
 import 'package:nextsense_trial_ui/domain/survey/adhoc_survey.dart';
+import 'package:nextsense_trial_ui/ui/components/header_text.dart';
+import 'package:nextsense_trial_ui/ui/components/medium_text.dart';
+import 'package:nextsense_trial_ui/ui/components/simple_button.dart';
 import 'package:nextsense_trial_ui/ui/navigation.dart';
+import 'package:nextsense_trial_ui/ui/nextsense_colors.dart';
 import 'package:nextsense_trial_ui/ui/screens/profile/profile_screen_vm.dart';
 import 'package:nextsense_trial_ui/ui/screens/survey/survey_screen.dart';
 import 'package:provider/src/provider.dart';
@@ -14,30 +18,24 @@ class StartAdhocSurveyDialog extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final profileViewModel = context.read<ProfileScreenViewModel>();
 
-    List<SimpleDialogOption> options = profileViewModel.getAdhocSurveys()
-        .map((survey) =>
-        SimpleDialogOption(
-          onPressed: () async {
+    List<Widget> options = profileViewModel.getAdhocSurveys().map((survey) =>
+        Padding(padding: EdgeInsets.all(15), child: SimpleButton(
+          text: MediumText(text: survey.name, color: NextSenseColors.darkBlue),
+          onTap: () async {
             bool completed = await _navigation.navigateTo(
                 SurveyScreen.id, arguments:
                 AdhocSurvey(survey, profileViewModel.studyId));
             Navigator.pop(context, completed);
-          },
-          child: Container(
-              color: Colors.blue,
-              padding: EdgeInsets.all(20.0),
-              child: Text(survey.name, style: TextStyle(
-                fontSize: 20, color: Colors.white
-              ),),
-          ),
+          }),
         )).toList();
 
     return SimpleDialog(
-      title: const Text('Select survey to start'),
+      title: HeaderText(text: 'Select survey to start'),
       children: options,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))),
     );
   }
 }
