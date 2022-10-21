02464062f007245d53bb2acfa695209e8ba49f34
Eric Bouchard <eric@nextsense.io>
2022-10-21 12:51:26 +0700

    Add error logging

--- nextsense_trial_ui/lib/managers/auth/google_auth_manager.dart
+++ nextsense_trial_ui/lib/managers/auth/google_auth_manager.dart
@@ -27,33 +27,35 @@ class GoogleAuthManager {
   String get authUid => _authUid ?? "";
 
   Future<AuthenticationResult> handleSignIn() async {
-      final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
-      if (googleSignInAccount == null) {
-        _logger.log(Level.WARNING, 'Could not authenticate with Google.');
+    _logger.log(Level.INFO, 'Starting Google sign-in.');
+    final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+    if (googleSignInAccount == null) {
+      _logger.log(Level.WARNING, 'Could not authenticate with Google.');
+      return AuthenticationResult.error;
+    }
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+    AuthCredential authCredential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+    _googleSignInAccount = _googleSignIn.currentUser;
+
+    // Authenticate in Firebase using the authentication credentials that were
+    // obtained from Google Auth.
+    try {
+      final UserCredential userCredential =
+          await _firebaseAuth.signInWithCredential(authCredential);
+      if (userCredential.user == null || userCredential.user!.isAnonymous) {
+        _logger.log(Level.SEVERE, "No user returned from Google Auth.");
         return AuthenticationResult.error;
       }
-      final GoogleSignInAuthentication googleSignInAuthentication =
-          await googleSignInAccount.authentication;
-      AuthCredential authCredential = GoogleAuthProvider.credential(
-        accessToken: googleSignInAuthentication.accessToken,
-        idToken: googleSignInAuthentication.idToken,
-      );
-      _googleSignInAccount = _googleSignIn.currentUser;
-      
-      // Authenticate in Firebase using the authentication credentials that were
-      // obtained from Google Auth.
-      try {
-        final UserCredential userCredential =
-            await _firebaseAuth.signInWithCredential(authCredential);
-        if (userCredential.user == null || userCredential.user!.isAnonymous) {
-          return AuthenticationResult.error;
-        }
-        _authUid = userCredential.user!.uid;
-      } on FirebaseAuthException catch (e) {
-        _logger.log(Level.SEVERE, e);
-        return AuthenticationResult.error;
-      }
-      return AuthenticationResult.success;
+      _authUid = userCredential.user!.uid;
+    } on FirebaseAuthException catch (e) {
+      _logger.log(Level.SEVERE, "Error when trying to authenticate with Google Auth: $e");
+      return AuthenticationResult.error;
+    }
+    return AuthenticationResult.success;
   }
 
   Future<void> handleSignOut() async {
