925f0816d89f9e84f1291701f3dc5d373a5b1c52
Eric Bouchard <eric@nextsense.io>
2023-03-03 17:33:00 +0700

    Add AppCheck test

--- nextsense_trial_ui/lib/managers/firebase_manager.dart
+++ nextsense_trial_ui/lib/managers/firebase_manager.dart
@@ -1,10 +1,21 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 class FirebaseManager {
   FirebaseApp? _firebaseApp;
 
   Future initializeFirebase() async {
     _firebaseApp = await Firebase.initializeApp();
+    if (kReleaseMode) {
+      await FirebaseAppCheck.instance.activate(
+        androidProvider: AndroidProvider.playIntegrity,
+      );
+    } else {
+      await FirebaseAppCheck.instance.activate(
+        androidProvider: AndroidProvider.debug,
+      );
+    }
   }
 
   FirebaseApp getFirebaseApp() {
