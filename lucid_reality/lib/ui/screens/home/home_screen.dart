fd52931c0b8f0a2cd3c1163d4034c8dcfea57607
jayesh@infocusp.in <jayesh@infocusp.in>
2024-01-10 17:52:00 +0530

    - Sleep chart added into dashboard.

--- lucid_reality/lib/ui/screens/home/home_screen.dart
+++ lucid_reality/lib/ui/screens/home/home_screen.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:lucid_reality/ui/components/app_card.dart';
 import 'package:lucid_reality/ui/components/app_circular_progress_indicator.dart';
+import 'package:lucid_reality/ui/components/sleep_pie_chart.dart';
+import 'package:lucid_reality/ui/components/wait_widget.dart';
 import 'package:lucid_reality/ui/screens/dashboard/dashboard_screen_vm.dart';
 import 'package:lucid_reality/ui/screens/home/home_container.dart';
 import 'package:lucid_reality/ui/screens/learn/Insight_learn.dart';
 import 'package:lucid_reality/ui/screens/lucid/lucid_screen_vm.dart';
 import 'package:lucid_reality/ui/screens/reality_check/reality_check_settings.dart';
+import 'package:lucid_reality/ui/screens/sleep/day_screen_vm.dart';
+import 'package:lucid_reality/ui/screens/sleep/sleep_screen_vm.dart';
 import 'package:lucid_reality/utils/text_theme.dart';
 import 'package:lucid_reality/utils/utils.dart';
 import 'package:stacked/stacked.dart';
@@ -54,18 +58,57 @@ class HomeScreen extends HookWidget {
     return [
       HomeContainer(
         title: 'YOUR SLEEP SUMMARY',
-        child: AppCard(
-          SizedBox(
-            height: 199,
-            width: double.maxFinite,
-            child: Center(
-              child: Text(
-                textAlign: TextAlign.center,
-                'Oops!\nNo Data to generate Sleep Summary',
-                style: Theme.of(context).textTheme.bodyCaption,
-              ),
-            ),
-          ),
+        child: ViewModelBuilder.reactive(
+          viewModelBuilder: () => DayScreenViewModel(),
+          onViewModelReady: (viewModel) => viewModel.init(),
+          builder: (context, viewModel, child) {
+            if (viewModel.initialised) {
+              if (viewModel.healthAppAuthorized &&
+                  (viewModel.sleepResultType == SleepResultType.sleepStaging ||
+                      viewModel.sleepResultType == SleepResultType.sleepTimeOnly)) {
+                return AppCard(
+                  Container(
+                    height: 199,
+                    child: Stack(
+                      children: [
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: Text("Total time\n${viewModel.totalSleepTime}")),
+                        if (viewModel.sleepResultType != SleepResultType.noData)
+                          Align(
+                              alignment: Alignment.center,
+                              child: Container(
+                                  height: 250,
+                                  child: SleepPieChart.withData(viewModel.chartSleepStages))),
+                        Align(
+                            alignment: Alignment.center,
+                            child: Text("${viewModel.sleepStartEndTime}")),
+                        Align(
+                            alignment: Alignment.topRight,
+                            child: Text(textAlign: TextAlign.right, "Time to sleep\nN/A")),
+                      ],
+                    ),
+                  ),
+                );
+              } else {
+                return AppCard(
+                  SizedBox(
+                    height: 199,
+                    width: double.maxFinite,
+                    child: Center(
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        'Oops!\nNo Data to generate Sleep Summary',
+                        style: Theme.of(context).textTheme.bodyCaption,
+                      ),
+                    ),
+                  ),
+                );
+              }
+            } else {
+              return WaitWidget(message: 'Loading sleep data...');
+            }
+          },
         ),
       ),
       HomeContainer(
