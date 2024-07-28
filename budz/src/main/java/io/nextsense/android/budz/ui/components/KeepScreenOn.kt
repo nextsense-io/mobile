68f07f810d3af0c5c1297eca1ae9a2b5f22a8fa8
Eric Bouchard <eric@nextsense.io>
2024-07-28 16:33:59 +0700

    Keep screen on when visualising the signal

---
+++ budz/src/main/java/io/nextsense/android/budz/ui/components/KeepScreenOn.kt
@@ -0,0 +1,16 @@
+package io.nextsense.android.budz.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalView
+
+@Composable
+fun KeepScreenOn() {
+    val currentView = LocalView.current
+    DisposableEffect(Unit) {
+        currentView.keepScreenOn = true
+        onDispose {
+            currentView.keepScreenOn = false
+        }
+    }
+}
