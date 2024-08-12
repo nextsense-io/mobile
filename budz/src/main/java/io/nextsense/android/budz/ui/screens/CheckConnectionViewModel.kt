b5473b6d255b0799ff2bf26faa125fa942610b16
Eric Bouchard <eric@nextsense.io>
2024-08-12 23:07:53 +0700

    Write sleep-wake classification results in a CSV

--- budz/src/main/java/io/nextsense/android/budz/ui/screens/CheckConnectionViewModel.kt
+++ budz/src/main/java/io/nextsense/android/budz/ui/screens/CheckConnectionViewModel.kt
@@ -1,12 +1,15 @@
 package io.nextsense.android.budz.ui.screens
 
+import android.content.Context
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.nextsense.android.budz.manager.AirohaDeviceManager
 import io.nextsense.android.budz.manager.SignalStateManager
 import javax.inject.Inject
 
 @HiltViewModel
 class CheckConnectionViewModel @Inject constructor(
+    @ApplicationContext context: Context,
     airohaDeviceManagerParam: AirohaDeviceManager,
     signalStateManagerParam: SignalStateManager
-): SignalVisualizationViewModel(airohaDeviceManagerParam, signalStateManagerParam)
+): SignalVisualizationViewModel(context, airohaDeviceManagerParam, signalStateManagerParam)
